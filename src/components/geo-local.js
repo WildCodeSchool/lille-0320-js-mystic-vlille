a1a01a7c3f9d396c15e94e5a1a1690b149b923a2
amjad <sammanamjad86@gmail.com>
2020-04-21 15:10:17 +0200

    change var to const in line 8

--- src/components/geo-local.js
+++ src/components/geo-local.js
@@ -3,23 +3,16 @@ import { withLeaflet } from "react-leaflet";
 import Locate from "leaflet.locatecontrol";
 
 class LocateControl extends Component {
-  
-
-
     componentDidMount() {
         const { options } = this.props;
-        var { map } = this.props.leaflet;
-        
+        const { map } = this.props.leaflet;
+
         const lc = new Locate(options);
         lc.addTo(map);
-      }
-    
-      render() {
-        
-    
+    }
+
+    render() {
         return null;
-      }
-
-
+    }
 }
 export default withLeaflet(LocateControl);
