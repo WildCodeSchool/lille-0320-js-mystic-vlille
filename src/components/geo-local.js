cfa3b97026ca2094623494ce5fd7ecdb48d1badc
amjad <sammanamjad86@gmail.com>
2020-04-22 09:38:24 +0200

    solve prettier prob

--- src/components/geo-local.js
+++ src/components/geo-local.js
@@ -3,16 +3,16 @@ import { withLeaflet } from "react-leaflet";
 import Locate from "leaflet.locatecontrol";
 
 class LocateControl extends Component {
-    componentDidMount() {
-        const { options } = this.props;
-        const { map } = this.props.leaflet;
+  componentDidMount() {
+    const { options } = this.props;
+    const { map } = this.props.leaflet;
 
-        const lc = new Locate(options);
-        lc.addTo(map);
-    }
+    const lc = new Locate(options);
+    lc.addTo(map);
+  }
 
-    render() {
-        return null;
-    }
+  render() {
+    return null;
+  }
 }
 export default withLeaflet(LocateControl);
