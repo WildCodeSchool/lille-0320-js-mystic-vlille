410b9f9c2ad8ef5245904a8cc7ff698938299661
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-04-28 10:13:41 +0200

    add conditions with anavailable on list

--- src/components/List.js
+++ src/components/List.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./List.scss";
 
-export default function List({ stations }) {
+export default function List({ stations, stationState }) {
   console.log(stations);
   return (
     <div>
       {stations.map((station) => {
         return (
-          <div className="list">
-            <h2 key={station.fields.libelle}>{station.fields.nom}</h2>
-            <div className="mesListes">
-              <p>Nombres vélos: {station.fields.nbvelosdispo}</p>
-              <p>Nombres places: {station.fields.nbplacesdispo}</p>
-            </div>
+          <div>
+            {!stationState(station) && (
+              <div className="list">
+                <h2 key={station.fields.libelle}>{station.fields.nom}</h2>
+                <div className="mesListes">
+                  <p>Nombres vélos: {station.fields.nbvelosdispo}</p>
+                  <p>Nombres places: {station.fields.nbplacesdispo}</p>
+                </div>
+              </div>
+            )}{" "}
           </div>
         );
       })}
+      )}
     </div>
   );
 }
