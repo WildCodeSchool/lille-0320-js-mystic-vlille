240a0f82fa0e3dc341175a863d19e7598b2549a3
JenniferFromanger <jennifer.mirre@yahoo.fr>
2020-05-05 08:51:14 +0200

    add cb on List

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./List.scss";
+import Cb from "../icons/Cb";
 
 export default function List({ stations, stationState }) {
   const changeIcon = (station) => {
@@ -30,7 +31,7 @@ export default function List({ stations, stationState }) {
         })
         .map((station) => {
           return (
-            <div>
+            <div className="contain">
               {!stationState(station) && (
                 <div className="list">
                   <div className="image">
@@ -49,8 +50,11 @@ export default function List({ stations, stationState }) {
                     <p>{station.distance} km</p>
                   </div>
                   <div className="mesListes">
-                    <p>Nombres vélos: {station.fields.nbvelosdispo}</p>
-                    <p>Nombres places: {station.fields.nbplacesdispo}</p>
+                    <div>
+                      <p>Nombres vélos: {station.fields.nbvelosdispo}</p>
+                      <p>Nombres places: {station.fields.nbplacesdispo}</p>
+                    </div>
+                    <p>{station.fields.type === "AVEC TPE" && <Cb />}</p>
                   </div>
                 </div>
               )}{" "}
