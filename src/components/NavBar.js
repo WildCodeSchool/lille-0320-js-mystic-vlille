9677f920029f2e796766f48302138393ebb42729
llaulier <lioraloche@yahoo.fr>
2020-05-13 09:37:18 +0200

    icons list and map are added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import AppBar from "@material-ui/core/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
+import MapIcon from "@material-ui/icons/Map";
 import SwitchButton from "@material-ui/core/Switch";
 import Typography from "@material-ui/core/Typography";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -19,6 +21,7 @@ const NavBar = () => {
             <Typography className={classes.title} variant="h6" noWrap>
               V'Lille App
             </Typography>
+            <FormatListBulletedIcon />
             {active ? (
               <Link to="/list">
                 <SwitchButton
@@ -34,6 +37,7 @@ const NavBar = () => {
                 />
               </Link>
             )}
+            <MapIcon />
           </Toolbar>
         </AppBar>
       </div>
